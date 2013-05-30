533702d2429a1d5028c2c133d51116db2efb0e57
Prabhu R <rprabhu.mca@gmail.com>
2013-05-31 01:40:27 +0530

    Fork me on Github fixes

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+copy /y *.html D:\Progs\ASF\Apache2.2\htdocs\tamdict
+xcopy /y css D:\Progs\ASF\Apache2.2\htdocs\tamdict\css\
+xcopy /y js D:\Progs\ASF\Apache2.2\htdocs\tamdict\js\
+xcopy /y json D:\Progs\ASF\Apache2.2\htdocs\tamdict\json\
+copy /y *.txt D:\Progs\ASF\Apache2.2\htdocs\tamdict
